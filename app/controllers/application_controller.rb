c6754e8f9e2af8889a29c070c87e8f9533e77e8c
Alouiscious Media <info@reneesoul.com>
2019-11-04 02:36:08 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,53 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    # set :views, Proc.new { File.join(root, "./views") }
     # register Sinatra::Flash
-    register Sinatra::ActiveRecordExtension
-    enable :sessions
+    # register Sinatra::ActiveRecordExtension
+    # enable :sessions
     # set :session_secret, ENV.fetch('SESSION_SECRET') 
   end
     
+  get '/' do
+    redirect to  'new'
+  end
 
+  get "/new" do
+    erb :'/index'
+  end
+
+  get '/signup' do
+    erb :'new'
+  end
+
+  post '/signup' do
+binding.pry
+    # @user = User.create(:username => params[:username], :password => params[:password], :email => params[:email])
+    @user = User.create(params[:user])
+    if @user.save
+      redirect "/success"
+    else
+      redirect "/failure"
+    end
+
+    @users = User.all
+   
+  end
+
+  get '/login' do
+    @users = User.all
+    erb :'/user/new'
+  end
+  
+  
+post '/login' do
+    @user = User.find_by(username: params[:username], password: params[:password])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect "/session/success"
+    else
+      redirect "/session/failure"
+    end
+  end
 
 end
--- config.ru
+++ config.ru
@@ -1,16 +1,14 @@
 require './config/environment'
 require 'sinatra'
 
-# RackMethodOverride
+
 
 if ActiveRecord::Migrator.needs_migration?
   raise 'Migrations are pending. Run `rake db:migrate` to resolve the issue.'
 end
 
 require_relative 'app/controllers/application_controller'
-require_relative 'app/controllers/session_controller'
-require_relative 'app/controllers/user_controller'
 
-use UserController
-use SessionController
+
+
 run ApplicationController
